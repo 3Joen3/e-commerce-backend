a931ebb7599c980f382acc7dfa233e857c9f460f
3Joen3 <joalevall@gmail.com>
2025-04-05 16:45:53 +0200

    Added Create ProductVariant method without Attributes/Options

--- Core/Entities/ProductEntities/ProductVariant.cs
+++ Core/Entities/ProductEntities/ProductVariant.cs
@@ -4,11 +4,24 @@ namespace Core.Entities.ProductEntities;
 
 public class ProductVariant : BaseEntity
 {
-    public required decimal Price { get; set; }
+    public decimal Price { get; set; }
     public decimal ComparePrice { get; set; }
-    public decimal Cost { get; set; }
     public string ImageUrl { get; set; } = string.Empty;
-    public required ICollection<ProductVariantAttribute> Attributes { get; set; }
+    public ICollection<ProductVariantAttribute> Attributes { get; set; } = [];
 
     public Guid ProductId { get; set; }
+
+    private ProductVariant() { }
+
+    private ProductVariant(decimal price, decimal comparePrice, string imageUrl = "", ICollection<ProductVariantAttribute>? attributes = null)
+    {
+        if (price < 0) throw new ArgumentException("Price cannot be negative.");
+
+        Price = price;
+        ComparePrice = comparePrice;
+        ImageUrl = imageUrl;
+        Attributes = attributes ?? [];
+    }
+
+    public static ProductVariant CreateWithoutAttributes(decimal price, decimal comparePrice = 0) => new(price, comparePrice);
 }
