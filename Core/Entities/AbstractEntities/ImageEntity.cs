a3c19c3d580c911afaca289fdd19cd83bae25a8f
3Joen3 <joalevall@gmail.com>
2025-04-06 22:06:29 +0200

    Changed the ImageEntity to use the WebAddress value object instead of string.

--- Core/Entities/AbstractEntities/ImageEntity.cs
+++ Core/Entities/AbstractEntities/ImageEntity.cs
@@ -1,7 +1,20 @@
+using Core.Utils;
+using Core.ValueObjects;
+
 namespace Core.Entities.AbstractEntities;
 
 public abstract class ImageEntity : BaseEntity
 {
-    public required string Url { get; set; }
-    public required string AltText { get; set; }
+    public WebAddress Url { get; set; }
+    public string AltText { get; set; } = string.Empty;
+
+    protected ImageEntity(string url, string altText)
+    {
+        Guard.AgainstNullOrWhiteSpace(altText, nameof(altText));
+
+        Url = new WebAddress(url);
+        AltText = altText;
+    }
+
+    protected ImageEntity() { Url = null!; }
 }
